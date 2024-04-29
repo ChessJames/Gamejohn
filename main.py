42b16659d027bae558123902d438aa0a1d38d9d3
ChessJames <153975630+ChessJames@users.noreply.github.com>
2024-04-28 19:46:15 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+#atempt
+
+import pygame as py
+import random as r
+import time 
+from absurdspace import *
+
+welcomeplayed = False
+
+def begining_of_game():
+    name = input("What is the name: ")
+    capname = name.capitalize()
+    print("WAKE UP "+ capname)
+    time.sleep(2)
+    print("sorry that is my mistake let me reset my words")
+    time.sleep(1)
+    welcome()
+
+def welcome():  
+    print(aspace)
+    print("I am John Check ")
+    time.sleep(2)
+    print("A silly guy to check on your gameplay exsperince")
+    time.sleep(2)
+    print("So many fun games to play")
+    time.sleep(2)
+    print("(   I made this as a excuse to gamble :)  )")
+    print ("lets begin")
+    time.sleep(2)
+
+
+
+
+  
+begining_of_game()
+
